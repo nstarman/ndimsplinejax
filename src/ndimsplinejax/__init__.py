a0e0162d7c85c85cc90799c7287f24e717246fa9
nstarman <nstarman@users.noreply.github.com>
2024-02-13 22:04:00 -0500

    feat: major simplification
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/ndimsplinejax/__init__.py
+++ src/ndimsplinejax/__init__.py
@@ -1,17 +1,12 @@
 """N-dimensional splines in JAX."""
 
-__all__ = [
-    "__version__",
-    "__version_tuple__",
-    "SplineCoefs_from_GriddedData",
-    "SplineInterpolant",
-]
+__all__ = ["__version__", "__version_tuple__", "compute_coeffs", "SplineInterpolant"]
 
 import jaxtyping
 
 with jaxtyping.install_import_hook("ndimsplinejax", "beartype.beartype"):
     from ._version import __version__, __version_tuple__
-    from .coeffs import SplineCoefs_from_GriddedData
+    from .coeffs import compute_coeffs
     from .core import SplineInterpolant
 
 
