a12bb905bdca9c815c4f7f0399a8e526b4156e72
nstarman <nstarman@users.noreply.github.com>
2024-02-13 22:49:34 -0500

    refactor spline interpolants
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/ndimsplinejax/__init__.py
+++ src/ndimsplinejax/__init__.py
@@ -1,13 +1,32 @@
 """N-dimensional splines in JAX."""
 
-__all__ = ["__version__", "__version_tuple__", "compute_coeffs", "SplineInterpolant"]
+__all__ = [
+    "__version__",
+    "__version_tuple__",
+    "compute_coeffs",
+    "AbstractSplineInterpolant",
+    "spline_interpolant",
+    "Spline1DInterpolant",
+    "Spline2DInterpolant",
+    "Spline3DInterpolant",
+    "Spline4DInterpolant",
+    "Spline5DInterpolant",
+]
 
 import jaxtyping
 
 with jaxtyping.install_import_hook("ndimsplinejax", "beartype.beartype"):
     from ._version import __version__, __version_tuple__
     from .coeffs import compute_coeffs
-    from .core import SplineInterpolant
+    from .core import (
+        AbstractSplineInterpolant,
+        Spline1DInterpolant,
+        Spline2DInterpolant,
+        Spline3DInterpolant,
+        Spline4DInterpolant,
+        Spline5DInterpolant,
+        spline_interpolant,
+    )
 
 
 # Clean up namespace
